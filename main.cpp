9eabcff460ba2f811ded9fef42746d81e20a2826
Andrew O <andrew.t.olguin@gmail.com>
2022-10-09 14:23:19 +0000

    Schedule implemented as linked list, adding ability for each event ot have up to three actions next

--- main.cpp
+++ main.cpp
@@ -2,12 +2,54 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <array>
+
+void four_hertz_task(double call_time){
+    std::cout << "I'm the first task, running at 4hz! The time (relative to start) is " << std::to_string(call_time) << std::endl; 
+}
+
+void five_hertz_task(double call_time){
+    std::cout << "I'm the second task, running at 5hz! The time (relative to start) is " << std::to_string(call_time) << std::endl; 
+}
+
+void three_hertz_task(double call_time){
+    std::cout << "I'm the third task, running at 3hz! The time (relative to start) is " << std::to_string(call_time) << std::endl; 
+}
+
+void two_hertz_task(double call_time){
+    std::cout << "I'm the third task, running at 2hz! The time (relative to start) is " << std::to_string(call_time) << std::endl; 
+}
+
+void print_schedule(Schedule& schedule){
+    for (auto &el : schedule.schedule){
+        std::cout << el.name << ": " << std::to_string(el.execution_time) << std::endl;
+    }
+}
 
 int main(int argc, char *argv[]){
     std::cout << "Hello World! I'm a C++ program!" << std::endl;
+    std::array<int, 3> hmm;
 
     struct timespec current_time;
     clock_gettime(CLOCK_REALTIME, &current_time);
     std::cout << "The time is roughly " << std::to_string(current_time.tv_sec) << std::endl;
+
+    ScheduleEvent test_event;
+    test_event.action = four_hertz_task;
+    test_event.action(123.0);
+
+    // build up a list of first _task events
+    Schedule my_schedule;
+
+    // run through the list & execute the schedule
+    my_schedule.add_to_schedule(5.0, "5hz task", std::function<void(double)>(five_hertz_task));
+    print_schedule(my_schedule);
+    std::cout << std::endl;
+    my_schedule.add_to_schedule(4.0, "4hz task", std::function<void(double)>(four_hertz_task));
+    print_schedule(my_schedule);
+    std::cout << std::endl;
+    my_schedule.add_to_schedule(2.0, "2hz task", std::function<void(double)>(two_hertz_task));
+    print_schedule(my_schedule);
+
     return 0;
 }
--- toy_scheduler.h
+++ toy_scheduler.h
@@ -1,10 +1,85 @@
 #include <forward_list>
+#include <functional>
+#include <string>
+#include <iostream>
 
+constexpr double event_precision = 0.001;
 struct ScheduleEvent {
     double execution_time;
-    double action; // fn pointer TODO
+    std::string name;
+    std::function<void(double)> action;
 };
 
 class Schedule {
+public:
     std::forward_list<ScheduleEvent> schedule;
+
+    void clear()
+    {
+        schedule.clear();
+    }
+
+    bool time_is_between_events(const double current_time, const std::forward_list<ScheduleEvent>::iterator& left_event, const std::forward_list<ScheduleEvent>::iterator& right_event){
+        if (current_time > (*left_event).execution_time && current_time < (*right_event).execution_time){
+            return true;
+        } else { 
+            return false;
+        }
+    }
+
+    void add_to_schedule(double freq, std::string event_name, std::function<void(double)> event) //TODO: account for offset
+    {
+        // add event to the schedule such that it is triggered at the desired frequency (assuming a 1hz total schedule frequency)
+        double schedule_time = 0.0;
+        double period = 1.0 / freq; // div/0 vulnerability
+        
+        ScheduleEvent event_to_add;
+        event_to_add.action = event;
+        event_to_add.name = event_name;
+        event_to_add.execution_time = schedule_time;
+    
+        auto schedule_item = schedule.before_begin();
+        auto schedule_next = schedule_item;
+        schedule_next++;
+
+        while (schedule_time < 1.0){
+            event_to_add.execution_time = schedule_time;
+
+            if (schedule_next == schedule.end()){
+                // catch end-of-list inserts
+                schedule.insert_after(schedule_item, event_to_add);
+                schedule_time += period;
+                schedule_item++;
+                continue;
+            } 
+
+            // traverse the schedule until we find a valid entry point
+            while(!time_is_between_events(schedule_time, schedule_item, schedule_next)){
+
+                // catch equality case
+                if ((*schedule_item).execution_time - schedule_time < event_precision) {
+                    // times are basically the same, put them in bois
+                    schedule_item++;
+                    break;
+
+                } else { // not between, not equal, just step forward
+                    schedule_item++;
+                    schedule_next++;
+                }
+
+                // breakout if we've hit the end of the list. Short Curcuiting makes putting this condition in the while evaluation a bug.
+                if (schedule_next == schedule.end()){
+                    break;
+                }
+            }
+
+            // we've found a valid place to insert the event
+            schedule.insert_after(schedule_item, event_to_add);
+            schedule_item++;
+            schedule_next++;
+
+
+            schedule_time += period;
+        }
+    }
 };
